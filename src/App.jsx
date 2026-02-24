5d59478d20ae892ae78df387daa1fde61383b311
Slaveyko Zafirov <slaveyko.zafirov@yahoo.com>
2026-02-24 14:45:32 +0000

    create Header component and added props username and avatar_url to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [signedInUser, setSignedInUser] = useState(null);
+  useEffect(() => {
+    fetch("https://back-end-nc-news-channel.onrender.com/api/users/")
+      .then((res) => res.json())
+      .then((data) => {
+        const user = data.users.find((u) => u.username === "grumpy19");
+        console.log("Loaded user:", user);
+        setSignedInUser(user);
+      })
+      .catch((err) => {
+        console.error("Failed to load user:", err);
+      });
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {signedInUser && (
+        <Header
+          username={signedInUser.username}
+          avatar_url={signedInUser.avatar_url}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+function Header({ username, avatar_url }) {
+  return (
+    <>
+      <h1 id="welcome">Welcome to NC - News</h1>
+      <div className="user-data">
+        <img
+          src={avatar_url}
+          alt={`${username}'s avatar`}
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "50%",
+            objectFit: "cover",
+            border: "2px solid #ddd",
+          }}
+        />
+        <span>{username}</span>
+      </div>
+    </>
+  );
+}
+
+export default Header;
